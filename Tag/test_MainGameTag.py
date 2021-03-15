18cb2bc406b33f0aea5ac4b5c4bcedd693d2209a
erojastor <erojast@calstatela.edu>
2021-03-14 21:43:56 -0700

    Create test_MainGameTag.py

---
+++ Tag/test_MainGameTag.py
@@ -0,0 +1,37 @@
+import pytest
+from unittest import TestCase
+from MainGameTag import GameSetup, Actions, Catch
+
+
+class TestGameSetup(TestCase):
+
+    def test_collision(self):
+        pass
+
+
+class TestActions(TestCase):
+    def player1_score(self):
+        pass
+
+    def player2_score(self):
+        pass
+
+    def _player1_(self):
+        pass
+
+    def _player2_(self):
+        pass
+
+
+class TestCatch(TestCase):
+    def test_catch(self):
+        pass
+
+
+class Game(TestCase):
+    def test_build(self):
+        pass
+
+
+if __name__ == '__main__':
+    pytest.main()
